029774ad0452bd15a2881234adcdcf7a73a32134
Chinedu Daniel <chinedudaniel7@gmail.com>
2023-10-05 11:36:27 +0200

    Setup Ignito service [status: incomplete, sn: 001]

--- apps/ignito/src/main.ts
+++ apps/ignito/src/main.ts
@@ -3,6 +3,7 @@ import {
   FastifyAdapter,
   NestFastifyApplication,
 } from '@nestjs/platform-fastify';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -10,6 +11,15 @@ async function bootstrap() {
     AppModule,
     new FastifyAdapter({ logger: true }),
   );
+  const config = new DocumentBuilder()
+    .setTitle('Ignito Gateway')
+    .setDescription('The core Ignito service')
+    .setVersion('1.0')
+    .addTag('ignito')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(process.env.GATEWAY_SERVICE_PORT as string, '0.0.0.0');
 }
 bootstrap();
